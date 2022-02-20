1ab5f959f2dae0932973eacb49fbb2c9e07a5e53
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2022-02-20 20:00:06 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,12 +1,41 @@
-document.onreadystatechange = function () {
-  let state = document.readyState;
-  if (state == "interactive") {
-    document.getElementById("contents").style.visibility = "hidden";
-    document.getElementById("load").style.visibility = "visible";
-  } else if (state == "complete") {
-    setTimeout(function () {
-      document.getElementById("load").style.visibility = "hidden";
-      document.getElementById("contents").style.visibility = "visible";
-    }, 2000);
+// document.onreadystatechange = function () {
+//   let state = document.readyState;
+//   if (state == "interactive") {
+//     document.getElementById("contents").style.visibility = "hidden";
+//     document.getElementById("load").style.visibility = "visible";
+//   } else if (state == "complete") {
+//     setTimeout(function () {
+//       document.getElementById("load").style.visibility = "hidden";
+//       document.getElementById("contents").style.visibility = "visible";
+//     }, 2000);
+//   }
+// };
+
+function isVisible(elem) {
+  let coords = elem.getBoundingClientRect();
+
+  let windowHeight = document.documentElement.clientHeight;
+
+  // верхний край элемента виден?
+  let topVisible = coords.top > 0 && coords.top < windowHeight;
+
+  // нижний край элемента виден?
+  let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+
+  return topVisible || bottomVisible;
+}
+
+function showVisible() {
+  for (let img of document.querySelectorAll("img")) {
+    let realSrc = img.dataset.src;
+    if (!realSrc) continue;
+
+    if (isVisible(img)) {
+      img.src = realSrc;
+      img.dataset.src = "";
+    }
   }
-};
+}
+
+setTimeout(showVisible, 2000);
+window.onscroll = showVisible;
